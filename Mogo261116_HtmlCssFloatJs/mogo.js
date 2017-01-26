643b8fc821514be93eda7b770c416b3f3575d3e6
lelik759 <llelik759@gmail.com>
2017-01-26 08:47:01 +0200

    Signed-off-by: lelik759 <llelik759@gmail.com>

--- Mogo261116_HtmlCssFloatJs/mogo.js
+++ Mogo261116_HtmlCssFloatJs/mogo.js
@@ -1,57 +1,93 @@
-"use strict"
 
-var sliderText=document.getElementsByClassName("slider-text");
-var arrowRight=document.querySelector(".arrow-right");
-var arrowleft=document.querySelector(".arrow-left");
+
 
 var i =0;
-arrowRight.addEventListener("click", goRight );
-arrowleft.addEventListener("click", goLeft );
 
 
-function goRight ( ) {
-	sliderText[i].style.display="none";
-	++i;
-	if (i>=sliderText.length) {
-		i=0
+
+
+function slider (dir, e ) {
+
+	var sliderText=e.target.parentElement.querySelectorAll(".slider-text");
+	// console.log(sliderText);
+	
+	
+
+	if (dir=="arrow-right") {
+		sliderText[i].style.display="none";
+		++i;
+		if (i>=sliderText.length) {
+			i=0
+		}
+		sliderText[i].style.display="block";
+   }
+
+   if (dir=="arrow-left") {
+
+	   	sliderText[i].style.display="none";
+		--i;
+		if (i<=0) {
+			i=sliderText.length-1;
+		}
+		sliderText[i].style.display="block";
 	}
-	sliderText[i].style.display="block";
+
 } 
 
 
-function goLeft ( ) {
-	sliderText[i].style.display="none";
-	--i;
-	if (i<=0) {
-		i=sliderText.length-1;
+
+
+
+var arrowRight=document.querySelectorAll(".arrow-right");
+console.log(arrowRight);
+
+for (var j = 0; j < arrowRight.length; j++) {
+	arrowRight[j].onclick=function(e) {
+		slider ("arrow-right", e );
 	}
-	sliderText[i].style.display="block";
-} 
-
-
-
-
-
-
-var j=1;
-document.onclick=function (event) {
-	 var target = event.target;
-
-	if(!target.hasAttribute("data-arrow") ) return;
-	j++;
-		if(j%2==0) 
-		target.style.transform="rotate(180deg)";
-	 		if(j%2==1)
-			target.style.transform="rotate(0deg)";
-
-		 
-	var id = target.getAttribute('data-toggle-id');
-    if (!id) return;
-
-    var elem = document.getElementById(id);
-
-    elem.hidden = !elem.hidden;
 }
+
+var arrowLeft=document.querySelectorAll(".arrow-left");
+console.log(arrowLeft);
+
+for (var j = 0; j < arrowLeft.length; j++) {
+	arrowLeft[j].onclick=function(e) {
+		slider ("arrow-left",e);
+	}
+}
+
+
+
+
+
+
+// var arrowleft=document.querySelectorAll(".arrow-left");
+
+// arrowleft.addEventListener("click", goLeft );
+
+
+// function goLeft ( ) {
+// 	sliderText[i].style.display="none";
+// 	--i;
+// 	if (i<=0) {
+// 		i=sliderText.length-1;
+// 	}
+// 	sliderText[i].style.display="block";
+// } 
+
+
+
+// menu open
+
+// document.onclick=function (event) {
+// 	 var target = event.target;
+// 	 if(!target.hasAttribute("data-arrow") ) return;
+// 	 target.classList.toggle("revert");
+		
+// 	 var id = target.getAttribute('data-id');
+//      if (!id) return;
+//      document.querySelector ("."+id).classList.toggle("show");
+//  }
 
 
 
