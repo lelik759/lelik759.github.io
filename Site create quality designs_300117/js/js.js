2e83c2c4cdbd1cec288cf515f99bc343a729bb22
lelik759 <llelik759@gmail.com>
2017-02-06 23:50:27 +0200

    Signed-off-by: lelik759 <llelik759@gmail.com>

---
+++ Site create quality designs_300117/js/js.js
@@ -0,0 +1,21 @@
+
+
+
+    $(document).ready(function(){
+      $('.filtering').slick({
+      	slidesToShow: 8,
+  		slidesToScroll: 2,
+  		autoplay:true
+       
+      });
+   
+  
+
+   
+      $('.slider').slick({
+      		аccessibility:true,
+      		dots:true,
+      		arrows:true,
+      		fade:true
+        });
+    });
