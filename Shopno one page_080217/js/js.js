94f5edeabd36fbd367833540b6352a71634764bb
lelik759 <llelik759@gmail.com>
2017-02-10 00:15:12 +0200

    Signed-off-by: lelik759 <llelik759@gmail.com>

--- Shopno one page_080217/js/js.js
+++ Shopno one page_080217/js/js.js
@@ -3,14 +3,16 @@
 var menuButton=document.querySelector(".header-nav__icon");
 var buttonClose=document.querySelector(".header-nav-hidden__icon");
 var headerNavHidden=document.querySelector(".header-nav-hidden");
-
-function openingMenu() {
+// показ меню
+function openingMenu() { 
+	menuButton.style.opacity="0";
 	headerNavHidden.style.right="0";
 }
-
+// скрытие меню
 function closingMenu() {
 	headerNavHidden.style.right="-100%";
-	headerNavHidden.style.transition="right 2s"
+	headerNavHidden.style.transition="right 2s";
+	menuButton.style.opacity="0.8";
 }
 
 menuButton.addEventListener("click",openingMenu);
