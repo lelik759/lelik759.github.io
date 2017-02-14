ed288f98435328cbba19bdf52848080028a34559
lelik759 <llelik759@gmail.com>
2017-02-14 22:55:07 +0200

    1
    1

--- Shopno one page_080217/js/js.js
+++ Shopno one page_080217/js/js.js
@@ -18,3 +18,28 @@ function closingMenu() {
 menuButton.addEventListener("click",openingMenu);
 buttonClose.addEventListener("click",closingMenu);
 
+
+
+// Открытие и закрытие popup в разделе main-service
+
+
+var mainServiceLink=document.body.querySelectorAll(".main-service-column-block-opacity-content__link");
+
+for (var i = 0 ; i < mainServiceLink.length; i++) {
+				mainServiceLink[i].onclick=function () {
+				var link=this.parentElement.parentElement.parentElement.querySelector(".main-service-column-block-popup");
+				link.classList.add("show");
+				
+			}			
+	
+}
+
+var iconClose=document.body.querySelectorAll(".main-service-column-block-popup-content__icon");
+
+for (var j = 0 ; j < iconClose.length; j++) {
+				iconClose[j].onclick=function () {
+				var popup=this.parentElement.parentElement.parentElement.querySelector(".main-service-column-block-popup");
+				popup.classList.remove("show");
+			}			
+	
+}
